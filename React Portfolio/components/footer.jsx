887c246db6a2a2e9685cf8a90da91b56e9cc7641
Phoebe <fergusonphoebe@gmail.com>
2025-03-20 18:01:15 -0400

    Footer

---
+++ React Portfolio/components/footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <a href="https://github.com/yourprofile" target="_blank" rel="noopener noreferrer">GitHub</a>
+      <a href="https://linkedin.com/in/yourprofile" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+      <a href="https://twitter.com/in/yourprofile" target="_blank" rel="noopener noreferrer">Twitter</a>
+
+    </footer>
+  );
+}
+
+export default Footer;
