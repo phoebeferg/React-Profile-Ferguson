0dc62b3dfddce547d58406481a8665fb93af75ac
Phoebe <fergusonphoebe@gmail.com>
2025-03-20 18:04:35 -0400

    App files

--- React Portfolio/src/App.jsx
+++ React Portfolio/src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "../components/header";
+import Footer from "../components/footer";
+import AboutMe from "../components/aboutMe";
+import Portfolio from "../components/portfolio";
+import Contact from "../components/contact";
+import Resume from "../components/resume";
+import "./app.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  console.log("App is rendering")
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <div className="app-container">
+        <Header />
+        <main>
+          <Routes>
+            <Route path="/about" element={<AboutMe />} />
+            <Route path="/portfolio" element={<Portfolio />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/resume" element={<Resume />} />
+            <Route path="/" element={<AboutMe />} />
+          </Routes>
+        </main>
+        <Footer />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
