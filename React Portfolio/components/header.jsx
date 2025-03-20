842cbabc7d2162961d80422a1689df3a2b492805
Phoebe <fergusonphoebe@gmail.com>
2025-03-20 18:01:35 -0400

    Header

---
+++ React Portfolio/components/header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navigation from "./navigation";
+import "./Header.css";
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>Phoebe Ferguson</h1>
+      <Navigation />
+    </header>
+  );
+}
+
+export default Header;
