55855d2f57d8941169b040ece836ff97ec28ee5c
Phoebe <fergusonphoebe@gmail.com>
2025-03-20 18:00:07 -0400

    About Me section

---
+++ React Portfolio/components/aboutMe.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./aboutMe.css";
+
+function AboutMe() {
+  return (
+    <section className="about-me">
+      <img src="https://tse1.mm.bing.net/th?id=OIP.HJ1qQumlqLH-YkQCUJ6XUgHaHa&pid=Api&P=0&h=220" alt="Developer" className="profile-pic" />
+      <p>Hello! I'm a web developer passionate about creating awesome web applications.</p>
+    </section>
+  );
+}
+
+export default AboutMe;
